58bc72b59230f743a921b3b484d02c385f4fdf7c
zgljl2012 <2693491512@qq.com>
2022-04-09 21:21:38 +0800

    split the handler of caption

--- plugins/extensions/image/image.ts
+++ plugins/extensions/image/image.ts
@@ -3,8 +3,9 @@ import utils from '../../utils'
 import { Toolbar } from './toolbar'
 import * as MediumEditor from 'medium-editor-x'
 
-const captionClassName = 'medium-editor-insert-image-caption'
-
+const captionClassName: string = 'medium-editor-insert-image-caption'
+const elementClassName: string = 'medium-editor-insert-images'
+const activeClassName: string = 'medium-editor-insert-image-active'
 export interface ImageOptions {
   onClick(cb: (url: string) => void)
 }
@@ -20,11 +21,8 @@ export class ImageExtension implements Extension {
   }
   _plugin: any
   _editor: any
+  elementClassName: string = elementClassName
 
-  // class name
-  elementClassName: string = 'medium-editor-insert-images'
-  activeClassName: string = 'medium-editor-insert-image-active'
-  captionClassName: string = captionClassName
   toolbar: any
   imageID: number = 0
   cacheImages: any = {}
@@ -39,9 +37,10 @@ export class ImageExtension implements Extension {
     this.events()
 
     // listen for editing figcaptions
-    this._plugin.on(document, 'keyup', this.captionListener.bind(this))
+    this._plugin.on(document, 'keyup', this.captionHandler.bind(this))
   }
 
+  // 处理在图片后面回车的情况
   private handleEnterAfterImageElem (elem: any): boolean {
     if (elem.previousSibling && elem.previousSibling.classList.contains(this.elementClassName)) {
       // 清除 elem 的 style，因为这个 style 是从 image 上复制过来的
@@ -58,71 +57,94 @@ export class ImageExtension implements Extension {
     return false
   }
 
-  private captionListener (event: any) {
-    if (event.keyCode === 13) {
-      const elem = MediumEditor.selection.getSelectionStart(this._editor.options.ownerDocument)
-      // 判断当前的上个元素是否为 img，处理从图片末尾回车和整个 figcaption 被删除的问题
-      if (this.handleEnterAfterImageElem(elem)) {
-        return
-      }
-      // 判断当前是否为 figcaption，即处理 span 被删除的问题
-      if (elem.tagName.toLowerCase() === 'figcaption') {
-        // 表明 figcaption 的内容被删除，需要删除此 figcaption
-        // 删除生成的兄弟节点，然后跳转到下一行（如果没有下一行就新增一行）
-        const el = elem.parentNode
-        el.removeChild(elem.previousSibling)
-        el.removeChild(elem)
+  // 处理 figcaption 中的 caption 都被删除，然后回车的情况
+  private handleEnterAfterDeleteCaption (elem: any): boolean {
+    if (elem.tagName.toLowerCase() === 'figcaption') {
+      // 表明 figcaption 的内容被删除，需要删除此 figcaption
+      // 删除生成的兄弟节点，然后跳转到下一行（如果没有下一行就新增一行）
+      const el = elem.parentNode
+      el.removeChild(elem.previousSibling)
+      el.removeChild(elem)
 
-        let next = el.nextSibling
-        if (!next) {
-          next = document.createElement('p')
-          next.innerHTML ='<br>'
-          el.parentNode.appendChild(next)
-        }
-        // move cursor
-        MediumEditor.selection.moveCursor(document, next, next.childNodes.length)
-        return
+      let next = el.nextSibling
+      if (!next) {
+        next = document.createElement('p')
+        next.innerHTML ='<br>'
+        el.parentNode.appendChild(next)
       }
-      // TODO 处理给 caption 做了加粗等处理的情况
-      // 如果是 span，同时 parent 是 font，parent.parent 是 figcaption，则是说明是先删除完 span 再重新输入的情况
-      if (elem.parentNode.tagName.toLowerCase() === 'font' && elem.parentNode.parentNode.tagName.toLowerCase() === 'figcaption') {
-        const imageID = elem.parentNode.parentNode.getAttribute('image-id')
-        let el = this.cacheImages[parseInt(imageID, 10)]
-        if (!el) {
-          return
-        }
-        if (el.classList.contains(this.elementClassName)) {
-          // 删除 el 的最后一个，移到下一行，如果没有的话，给 el 的父节点添加新行
-          if (el.lastChild.nodeName.toLowerCase() === 'p') {
-            el.removeChild(el.lastChild)
-          }
-          utils.moveToNext(el)
-        }
-        return
-      }
-      // 判断当前是否为 figcaption -> span
-      if (!elem || !elem.classList.contains(this.captionClassName)) {
-        return
-      }
-      // get the image ID
-      const imageID = elem.getAttribute('image-id')
-      if (!imageID) {
-        return
-      }
+      // move cursor
+      MediumEditor.selection.moveCursor(document, next, next.childNodes.length)
+      return true
+    }
+    return false
+  }
+
+  // 处理删除后重新输入的情况
+  private handleEnterAfterReInputCaption (elem: any): boolean {
+    // 如果是 span，同时 parent 是 font，parent.parent 是 figcaption，则是说明是先删除完 span 再重新输入的情况
+    if (elem.parentNode.tagName.toLowerCase() === 'font' && elem.parentNode.parentNode.tagName.toLowerCase() === 'figcaption') {
+      const imageID = elem.parentNode.parentNode.getAttribute('image-id')
       let el = this.cacheImages[parseInt(imageID, 10)]
       if (!el) {
-        return
+        return false
       }
-      if (el.classList.contains(this.elementClassName)) {
+      if (el.classList.contains(elementClassName)) {
         // 删除 el 的最后一个，移到下一行，如果没有的话，给 el 的父节点添加新行
         if (el.lastChild.nodeName.toLowerCase() === 'p') {
           el.removeChild(el.lastChild)
         }
         utils.moveToNext(el)
       }
-      event.preventDefault()
+      return true
+    }
+    return false
+  }
+
+  // 处理在 caption 后面回车的情况
+  private handleEnterAfterCaption (elem: any): boolean {
+    // 如不是 caption 元素，则直接退出
+    if (!elem || !elem.classList.contains(captionClassName)) {
+      return false
+    }
+    // get the image ID
+    const imageID = elem.getAttribute('image-id')
+    if (!imageID) {
+      return false
+    }
+    // 获取图片元素
+    let el = this.cacheImages[parseInt(imageID, 10)]
+    if (!el) {
+      return false
+    }
+    if (el.classList.contains(elementClassName)) {
+      // 此时在 image 中新增了一行，删除此行
+      if (el.lastChild.nodeName.toLowerCase() === 'p') {
+        el.removeChild(el.lastChild)
+      }
+      // 移到下一行或新行
+      utils.moveToNext(el)
     }
     return true
+  }
+
+  private captionHandler (event: any) {
+    // TODO caption 的全选删除，删除时，不能把下一行的内容填充到 fagcaption 中
+    // TODO 不允许变化 caption 的 style
+    if (event.keyCode === 13) {
+      const elem = MediumEditor.selection.getSelectionStart(this._editor.options.ownerDocument)
+      // 判断当前的上个元素是否为 img，处理从图片末尾回车和整个 figcaption 被删除的问题
+      const handlers: ((elem: any) => boolean)[] = [
+        this.handleEnterAfterImageElem.bind(this),
+        this.handleEnterAfterDeleteCaption.bind(this),
+        this.handleEnterAfterReInputCaption.bind(this),
+        this.handleEnterAfterCaption.bind(this)
+      ]
+      for(const handler of handlers) {
+        if (handler(elem)) {
+          return
+        }
+      }
+    }
   }
 
   private nextImageID () {
@@ -152,7 +174,7 @@ export class ImageExtension implements Extension {
     this.toolbar = new Toolbar({
       plugin: this._plugin,
       type: 'images',
-      activeClassName: this.activeClassName,
+      activeClassName: activeClassName,
       buttons: [
         {
           name: 'align-left',
@@ -217,7 +239,7 @@ export class ImageExtension implements Extension {
     // caption
     const caption = document.createElement('figcaption')
     caption.setAttribute('image-id', `${imageID}`)
-    caption.innerHTML = `<span image-id='${imageID}' class="${this.captionClassName}">请输入图片描述</span>`
+    caption.innerHTML = `<span image-id='${imageID}' class="${captionClassName}">请输入图片描述</span>`
 
     // If we're dealing with a preview image,
     // we don't have to preload it before displaying
@@ -235,7 +257,7 @@ export class ImageExtension implements Extension {
       }
       domImage.src = url
     }
-    el.classList.add(this.elementClassName)
+    el.classList.add(elementClassName)
     el.contenteditable = false
     // Return domImage so we can test this function easily (just for testing)
     return domImage
@@ -248,7 +270,7 @@ export class ImageExtension implements Extension {
       el.nodeName.toLowerCase() === 'img'
       // && this.getClosestWithClassName(el, this.elementClassName)
     ) {
-      el.classList.add(this.activeClassName)
+      el.classList.add(activeClassName)
       this._editor.selectElement(el)
     }
   }
@@ -260,18 +282,18 @@ export class ImageExtension implements Extension {
     // Unselect all selected images. If an image is clicked, unselect all except this one.
     if (
       el.nodeName.toLowerCase() === 'img' &&
-      el.classList.contains(this.activeClassName)
+      el.classList.contains(activeClassName)
     ) {
       clickedImage = el
     }
 
     const images = utils.getElementsByClassName(
       this._plugin.getEditorElements(),
-      this.activeClassName
+      activeClassName
     )
     Array.prototype.forEach.call(images, image => {
       if (image !== clickedImage) {
-        image.classList.remove(this.activeClassName)
+        image.classList.remove(activeClassName)
       }
     })
   }
