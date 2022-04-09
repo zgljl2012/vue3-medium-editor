f44b2295b6d8c6fac9b2801ccb101309e89e9fec
zgljl2012 <2693491512@qq.com>
2022-04-09 20:52:23 +0800

    handleEnterAfterImageElem

--- plugins/extensions/image/image.ts
+++ plugins/extensions/image/image.ts
@@ -26,9 +26,7 @@ export class ImageExtension implements Extension {
   activeClassName: string = 'medium-editor-insert-image-active'
   captionClassName: string = captionClassName
   toolbar: any
-  captionListener: any
   imageID: number = 0
-
   cacheImages: any = {}
 
   constructor(plugin: any, options: ImageOptions) {
@@ -41,64 +39,54 @@ export class ImageExtension implements Extension {
     this.events()
 
     // listen for editing figcaptions
-    this.captionListener = (event: any) => {
-      if (event.keyCode === 13) {
-        const elem = MediumEditor.selection.getSelectionStart(this._editor.options.ownerDocument)
-        // 判断当前是否为 img，即处理整个 figcaption 被删除的问题
-        if (elem.previousSibling && elem.previousSibling.classList.contains(this.elementClassName)) {
-          // 清除 elem 的 style，因为这个 style 是从 image 上复制过来的
-          elem.style = ''
-          // 如果 elem 中包含 figcaption，则将 figcaption 移回 img 中
-          if (elem.childNodes[1] && elem.childNodes[1].tagName.toLowerCase() === 'figcaption') {
-            elem.previousSibling.appendChild(elem.childNodes[1])
-            elem.removeChild(elem.childNodes[1])
-          }
-          return
-        }
-        // 判断当前是否为 figcaption，即处理 span 被删除的问题
-        if (elem.tagName.toLowerCase() === 'figcaption') {
-          // 表明 figcaption 的内容被删除，需要删除此 figcaption
-          // 删除生成的兄弟节点，然后跳转到下一行（如果没有下一行就新增一行）
-          const el = elem.parentNode
-          el.removeChild(elem.previousSibling)
-          el.removeChild(elem)
+    this._plugin.on(document, 'keyup', this.captionListener.bind(this))
+  }
 
-          let next = el.nextSibling
-          if (!next) {
-            next = document.createElement('p')
-            next.innerHTML ='<br>'
-            el.parentNode.appendChild(next)
-          }
-          // move cursor
-          MediumEditor.selection.moveCursor(document, next, next.childNodes.length)
-          return
+  private handleEnterAfterImageElem (elem: any): boolean {
+    if (elem.previousSibling && elem.previousSibling.classList.contains(this.elementClassName)) {
+      // 清除 elem 的 style，因为这个 style 是从 image 上复制过来的
+      elem.style = ''
+      // 清除 classList
+      elem.classList = []
+      // 如果 elem 中包含 figcaption，则将 figcaption 移回 img 中
+      if (elem.childNodes[1] && elem.childNodes[1].tagName.toLowerCase() === 'figcaption') {
+        elem.previousSibling.appendChild(elem.childNodes[1])
+        elem.removeChild(elem.childNodes[1])
+      }
+      return true
+    }
+    return false
+  }
+
+  private captionListener (event: any) {
+    if (event.keyCode === 13) {
+      const elem = MediumEditor.selection.getSelectionStart(this._editor.options.ownerDocument)
+      // 判断当前的上个元素是否为 img，处理从图片末尾回车和整个 figcaption 被删除的问题
+      if (this.handleEnterAfterImageElem(elem)) {
+        return
+      }
+      // 判断当前是否为 figcaption，即处理 span 被删除的问题
+      if (elem.tagName.toLowerCase() === 'figcaption') {
+        // 表明 figcaption 的内容被删除，需要删除此 figcaption
+        // 删除生成的兄弟节点，然后跳转到下一行（如果没有下一行就新增一行）
+        const el = elem.parentNode
+        el.removeChild(elem.previousSibling)
+        el.removeChild(elem)
+
+        let next = el.nextSibling
+        if (!next) {
+          next = document.createElement('p')
+          next.innerHTML ='<br>'
+          el.parentNode.appendChild(next)
         }
-        // TODO 处理给 caption 做了加粗等处理的情况
-        // 如果是 span，同时 parent 是 font，parent.parent 是 figcaption，则是说明是先删除完 span 再重新输入的情况
-        if (elem.parentNode.tagName.toLowerCase() === 'font' && elem.parentNode.parentNode.tagName.toLowerCase() === 'figcaption') {
-          const imageID = elem.parentNode.parentNode.getAttribute('image-id')
-          let el = this.cacheImages[parseInt(imageID, 10)]
-          if (!el) {
-            return
-          }
-          if (el.classList.contains(this.elementClassName)) {
-            // 删除 el 的最后一个，移到下一行，如果没有的话，给 el 的父节点添加新行
-            if (el.lastChild.nodeName.toLowerCase() === 'p') {
-              el.removeChild(el.lastChild)
-            }
-            utils.moveToNext(el)
-          }
-          return
-        }
-        // 判断当前是否为 figcaption -> span
-        if (!elem || !elem.classList.contains(this.captionClassName)) {
-          return
-        }
-        // get the image ID
-        const imageID = elem.getAttribute('image-id')
-        if (!imageID) {
-          return
-        }
+        // move cursor
+        MediumEditor.selection.moveCursor(document, next, next.childNodes.length)
+        return
+      }
+      // TODO 处理给 caption 做了加粗等处理的情况
+      // 如果是 span，同时 parent 是 font，parent.parent 是 figcaption，则是说明是先删除完 span 再重新输入的情况
+      if (elem.parentNode.tagName.toLowerCase() === 'font' && elem.parentNode.parentNode.tagName.toLowerCase() === 'figcaption') {
+        const imageID = elem.parentNode.parentNode.getAttribute('image-id')
         let el = this.cacheImages[parseInt(imageID, 10)]
         if (!el) {
           return
@@ -110,12 +98,31 @@ export class ImageExtension implements Extension {
           }
           utils.moveToNext(el)
         }
+        return
       }
-      return true
+      // 判断当前是否为 figcaption -> span
+      if (!elem || !elem.classList.contains(this.captionClassName)) {
+        return
+      }
+      // get the image ID
+      const imageID = elem.getAttribute('image-id')
+      if (!imageID) {
+        return
+      }
+      let el = this.cacheImages[parseInt(imageID, 10)]
+      if (!el) {
+        return
+      }
+      if (el.classList.contains(this.elementClassName)) {
+        // 删除 el 的最后一个，移到下一行，如果没有的话，给 el 的父节点添加新行
+        if (el.lastChild.nodeName.toLowerCase() === 'p') {
+          el.removeChild(el.lastChild)
+        }
+        utils.moveToNext(el)
+      }
+      event.preventDefault()
     }
-    window.removeEventListener('keyup', this.captionListener)
-    window.addEventListener('keyup', this.captionListener)
-    // end.
+    return true
   }
 
   private nextImageID () {
@@ -229,7 +236,7 @@ export class ImageExtension implements Extension {
       domImage.src = url
     }
     el.classList.add(this.elementClassName)
-    el.contentediable = false
+    el.contenteditable = false
     // Return domImage so we can test this function easily (just for testing)
     return domImage
   }
